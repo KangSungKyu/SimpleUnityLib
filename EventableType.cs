ffe74e28cf851ade204ff35533be93d274e70348
KangSungKyu <tjdrb70@gmail.com>
2019-10-17 17:59:16 +0900

    Add files via upload
    간단하게 value가 변하기 전,후에 대해서 event처리 해주는거 묶은거

---
+++ EventableType.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventableType<TValueType> //call OnChanged when variable's value changed
+{
+    public event System.Action<TValueType> OnChangedBefore = null;
+    public event System.Action<TValueType> OnChangedAfter = null;
+
+    public EventableType() => value = default;
+    public EventableType(TValueType v) => value = v;
+    //eventable<T> e = t;
+    public static implicit operator EventableType<TValueType>(TValueType v) { return new EventableType<TValueType>(v); }
+    //T t = e;
+    public static implicit operator TValueType(EventableType<TValueType> e) { return e.value; }
+
+    public override string ToString() { return value.ToString(); }
+
+    private TValueType value
+    {
+        get { return this.value; }
+        set
+        {
+            oldValue = this.value;
+            OnChangedBefore?.Invoke(oldValue);
+            this.value = value;
+            OnChangedAfter?.Invoke(this.value);
+        }
+    }
+    private TValueType oldValue = default;
+}
+
+/*
+public class TestClass
+{
+    EventableType<int> testValue = 0;
+
+    TestClass()
+    {
+        testValue.OnChangedBefore += (value) => { System.Console.WriteLine("changed before : {0}", value); };
+        testValue.OnChangedAfter += (value) => { System.Console.WriteLine("changed after : {0}", value); };
+        testValue = 10;
+
+        //in console :
+        //changed before : 0
+        //changed after : 10
+    }
+}
+//*/
