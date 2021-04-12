d67685998a6c39577ef4f1502fa3e29732737e2f
KangSungKyu <tjdrb70@gmail.com>
2021-04-12 11:36:11 +0900

    Update EventableType.cs

--- EventableType.cs
+++ EventableType.cs
@@ -1,49 +1,87 @@
-using UnityEngine;
-using System.Collections;
-
+using System;
 
 public class EventableType<TValueType> //call OnChanged when variable's value changed
 {
-    public event System.Action<TValueType> OnChangedBefore = null;
-    public event System.Action<TValueType> OnChangedAfter = null;
+    public event Action<TValueType> OnChangedBefore = null;
+    public event Action<TValueType> OnChangedAfter = null;
 
     public EventableType() => originValue = default;
     public EventableType(TValueType v) => originValue = v;
     //eventable<T> e = t;
-    public static implicit operator EventableType<TValueType>(TValueType v) { return new EventableType<TValueType>(v); }
-    //T t = e;
-    public static implicit operator TValueType(EventableType<TValueType> e) { return e.originValue; }
-
-    public override string ToString() { return originValue.ToString(); }
-
-    private TValueType originValue
+    public static implicit operator EventableType<TValueType>(TValueType v)
     {
-        get { return originValue; }
-        set
-        {
-            oldValue = originValue;
-            OnChangedBefore?.Invoke(oldValue);
-            originValue = value;
-            OnChangedAfter?.Invoke(originValue);
-        }
+        return new EventableType<TValueType>(v);
     }
+    //T t = e;
+    public static implicit operator TValueType(EventableType<TValueType> e)
+    {
+        return e.originValue;
+    }
+
+    public override string ToString()
+    {
+        return originValue.ToString();
+    }
+
+    public void AddChangedBeforeEvent(Action<TValueType> before)
+    {
+        Delegate[] invocationList = null;
+        
+        bool isContain = false;
+
+        if (OnChangedBefore != null)
+        {
+            invocationList = OnChangedBefore.GetInvocationList();
+
+            foreach (var invocation in invocationList)
+            {
+                if (invocation.Equals(before) == true)
+                {
+                    isContain = true;
+                    Debug.LogError("dont add contained event dispatcher");
+                    break;
+                }
+            }
+        }
+
+        if(isContain == false)
+            OnChangedBefore += before;
+    }
+
+    public void AddChangedAfterEvent(Action<TValueType> after)
+    {
+        Delegate[] invocationList = null;
+
+        bool isContain = false;
+
+        if (OnChangedAfter != null)
+        {
+            invocationList = OnChangedAfter.GetInvocationList();
+
+            foreach (var invocation in invocationList)
+            {
+                if (invocation.Equals(after) == true)
+                {
+                    isContain = true;
+                    Debug.LogError("dont add contained event dispatcher");
+                    break;
+                }
+            }
+        }
+
+        if (isContain == false)
+            OnChangedAfter += after;
+    }
+
+    public void Dirty(TValueType value)
+    {
+        oldValue = originValue;
+        OnChangedBefore?.Invoke(oldValue);
+        originValue = value;
+        OnChangedAfter?.Invoke(originValue);
+    }
+
+
+    private TValueType originValue = default;
     private TValueType oldValue = default;
 }
-
-/*
-public class TestClass
-{
-    EventableType<int> testValue = 0;
-
-    TestClass()
-    {
-        testValue.OnChangedBefore += (value) => { System.Console.WriteLine("changed before : {0}", value); };
-        testValue.OnChangedAfter += (value) => { System.Console.WriteLine("changed after : {0}", value); };
-        testValue = 10;
-
-        //in console :
-        //changed before : 0
-        //changed after : 10
-    }
-}
-//*/
